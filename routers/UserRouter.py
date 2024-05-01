3deb6b4da07820dfe6900fc66e82839974d8a86c
SOHIOIIIA <83229310+SOHIOIIIA@users.noreply.github.com>
2024-05-01 15:28:25 +0300

    Так, тут добавлен свой юзер

--- routers/UserRouter.py
+++ routers/UserRouter.py
@@ -7,10 +7,12 @@ import json
 from sqlalchemy.orm import Session
 from fastapi import HTTPException, UploadFile
 from database.PostgresDb import get_connection
-from dto.User import ResponseUser, UpUser
+from dto.User import ResponseUser, UpUser, AnswerUser
 from models.Models import Users, Towns
 from services import UsersService,ItemService
 from services.ItemService import get_items_by_user_id,add_photos_to_item
+from services.ItemsPhotosService import get_first_photo_by_id
+from services.MinioService import get_photo
 from services.UsersService import get_current_user
 
 router = APIRouter()
@@ -21,11 +23,15 @@ con_dependency = Annotated[Session, Depends(get_connection)]
 async def get_user(con: con_dependency, id: UUID):
     items = await get_items_by_user_id(id, con)
     user = con.query(Users).filter(Users.id==id).first()
-    return ResponseUser(
+    photo = get_first_photo_by_id(user.id, con)
+    f_p = str(photo.id_lots)
+    s_p = str(photo.photo)
+    photo_url = await get_photo(f"{f_p}/{s_p}")
+    return AnswerUser(
         name=user.name,
         contact=user.contact,
         datereg=user.datereg,
-        photo=user.photo,
+        photo=photo_url,
         town= con.query(Towns).filter(user.id_town == Towns.id).scalar(),
         items=items
     )
@@ -65,6 +71,22 @@ async def update_inf(
     user_data = json.loads(user_json)
     user_new = UpUser(**user_data)
     return await UsersService.uppdate_user(con,user,user_new,town)
+
+@router.get('/', tags=['user'])
+async def get_user(con: con_dependency, user: Users = Depends(get_current_user)):
+    items = await get_items_by_user_id(user.id, con)
+    photo = get_first_photo_by_id(user.id, con)
+    f_p = str(photo.id_lots)
+    s_p = str(photo.photo)
+    photo_url = await get_photo(f"{f_p}/{s_p}")
+    return AnswerUser(
+        name=user.name,
+        contact=user.contact,
+        datereg=user.datereg,
+        photo=photo_url,
+        town= con.query(Towns).filter(user.id_town == Towns.id).scalar(),
+        items=items
+    )
 
 @router.put('/photo/{id}', tags=['user'])
 async def update_photo(
